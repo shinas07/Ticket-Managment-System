351bc29aaf93dcfa49e62c62e4305278821a68fd
shinas07 <shinasamanpk@gmail.com>
2025-01-17 22:14:58 +0530

    refactor: extract permissions to module and add auto ticket creator

--- backend/tickets/views.py
+++ backend/tickets/views.py
@@ -2,12 +2,7 @@ from rest_framework import viewsets, permissions, filters
 from django_filters.rest_framework import DjangoFilterBackend
 from .models import Ticket
 from .serializers import TicketSerializer
-
-class IsOwnerOrStaffOrReadOnly(permissions.BasePermission):
-    def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS:
-            return True
-        return obj.created_by == request.user or request.user.is_staff
+from .permissions import IsOwnerOrStaffOrReadOnly
 
 class TicketViewSet(viewsets.ModelViewSet):
     serializer_class = TicketSerializer
@@ -22,3 +17,8 @@ class TicketViewSet(viewsets.ModelViewSet):
         if user.is_staff:
             return Ticket.objects.all()
         return Ticket.objects.filter(created_by=user)
+
+    def perform_create(self, serializer):
+        serializer.save(created_by=self.request.user)
+    
+# User Ticket Raise
