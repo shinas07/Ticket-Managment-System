4c42f40dd0f9eb66885f01dab11ec2a87c4beb3b
shinas07 <shinasamanpk@gmail.com>
2025-01-18 17:10:21 +0530

    feat(urls): Add user profile and logout routes, update user management paths

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import LoginView, UserListView,UserCreateView,BlockUserView,RefreshTokenView
+from .views import LoginView, UserListView,UserCreateView,BlockUserView,RefreshTokenView,UserProfileView,LogoutView
 
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
+    path('profile/',UserProfileView.as_view(), name='user-profile'),
     path('users/create/',UserCreateView.as_view(),name='user-create'),
     path('users/list/', UserListView.as_view(), name='user-list'),
     path('users/<int:user_id>/block/', BlockUserView.as_view(), name='block-user'),
-    path('refresh/', RefreshTokenView.as_view(), name='token_refresh'), 
+    path('refresh/', RefreshTokenView.as_view(), name='token_refresh'),
+    path('logout/', LogoutView.as_view(), name='auth_logout'), 
 ]
