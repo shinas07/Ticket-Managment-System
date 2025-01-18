6830584035c8b77d97b5d3c5bfab3b8012b4a8c7
shinas07 <shinasamanpk@gmail.com>
2025-01-18 17:34:55 +0530

    feat: Add routing for public and protected routes with role-based access

--- Frontend/src/routes/routes.jsx
+++ Frontend/src/routes/routes.jsx
@@ -1,50 +1,91 @@
 import { createBrowserRouter } from 'react-router-dom';
 import LandingPage from '../pages/LandingPage';
-// import Login from '../pages/auth/Login';
-
-
-const ProtectedLoggedInRoute = ({ element }) => {
-    const { user } = useAuth();
-    
-    if (user) {
-      return user.user_type === 'manager' 
-        ? <Navigate to="/manager/dashboard" /> 
-        : <Navigate to="/employee/dashboard" />;
-    }
-    return element;
-  };
-  
-  // Protected route for manager-only pages
-  const ProtectedManagerRoute = ({ element }) => {
-    // const { user } = useAuth();
-    
-    // if (!user) {
-    //   return <Navigate to="/login" />;
-    // }
-    
-    // if (user.user_type !== 'manager') {
-    //   return <Navigate to="/employee/dashboard" />;
-    // }
-    
-    return element;
-  };
-
-  
+import Login from '../pages/auth/Login';
+import UserDashboard from '../pages/dashboard/UserDashboard';
+import AdminDashboard from '../pages/dashboard/AdminDashboard';
+import TicketManagement from '../pages/tickets/UserTicketCreate';
+import UserTicketManagement from '../pages/tickets/UserTickets';
+import AdminUserManagement from '../pages/admin/UserManagement';
+import AdminTicketManagement from '../pages/admin/TicketManagement';
+import Unauthorized from '../pages/Unauthorized';
+import NotFound from '../pages/NotFound';
+import PublicRoute from './PublicRoute';
+import ProtectedRoute from './ProtectedRoute';
 
 const route = createBrowserRouter([
-    {
-      path: '/',
-      element: <LandingPage />,
-    },
-    // auth routes
-    //  {
-    //   path: '/login',
-    //   element: <Login />,
-    // },
-    // {
-    //   path: '*',
-    //   element: <NotFoundPage />,
-    // },
-  ]);
+  {
+    path: '/',
+    element: (
+      <PublicRoute>
+      <LandingPage />
+      </PublicRoute>
+    ),
+  },
+  // auth routes
+  {
+    path: '/login',
+    element: (
+      <PublicRoute>
+        <Login />
+      </PublicRoute>
+    ),
+  },
+  {
+    path: '/user/dashboard',
+    element: (
+      <ProtectedRoute allowedRoles={['user']}>
+        <UserDashboard />
+      </ProtectedRoute>
+    ),
+  },
+  {
+    path: '/user/ticket/create',
+    element: (
+      <ProtectedRoute allowedRoles={['user']}>
+        <TicketManagement />
+      </ProtectedRoute>
+    ),
+  },
+  {
+    path: '/user/ticket/manage',
+    element: (
+      <ProtectedRoute allowedRoles={['user']}>
+        <UserTicketManagement />
+      </ProtectedRoute>
+    ),
+  },
+  {
+    path: '/admin/dashboard',
+    element: (
+      <ProtectedRoute allowedRoles={['admin']}>
+        <AdminDashboard />
+      </ProtectedRoute>
+    ),
+  },
+  {
+    path: '/admin/user/manage',
+    element: (
+      <ProtectedRoute allowedRoles={['admin']}>
+        <AdminUserManagement />
+      </ProtectedRoute>
+    ),
+  },
+  {
+    path: '/admin/tickets/management',
+    element: (
+      <ProtectedRoute allowedRoles={['admin']}>
+        <AdminTicketManagement />
+      </ProtectedRoute>
+    ),
+  },
+  {
+    path: '/unauthorized',
+    element: <Unauthorized />,
+  },
+  {
+    path: '*',
+    element: <NotFound />,
+  },
+]);
 
 export default route;
