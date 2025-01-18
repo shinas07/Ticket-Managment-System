4bebf811c81614a657fc9ec4f44a2821493088ea
shinas07 <shinasamanpk@gmail.com>
2025-01-18 17:09:52 +0530

    chore(urls): Register ticket API endpoint and update authentication paths

--- backend/ticket_system/urls.py
+++ backend/ticket_system/urls.py
@@ -18,13 +18,12 @@ from django.contrib import admin
 from django.urls import path,include
 from rest_framework.routers import DefaultRouter
 from tickets.views import TicketViewSet
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-
+# Create a router and register our viewset
+router = DefaultRouter()
+router.register(r'tickets', TicketViewSet, basename='ticket')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/',include('accounts.urls')),
-    # path('api/', include(router.urls)),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/', include(router.urls)),
 ]
