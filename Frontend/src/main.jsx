28b2695749eb5dd387b982fabb55872a805cb01e
shinas07 <shinasamanpk@gmail.com>
2025-01-19 18:43:03 +0530

    style: format imports and JSX for consistent code style

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,15 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { Toaster } from 'sonner'
-import { AuthProvider } from './context/AuthContext.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { Toaster } from "sonner";
+import { AuthProvider } from "./context/AuthContext.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Toaster position="top-right" richColors expand={false}/>
+    <Toaster position="top-right" richColors expand={false} />
     <AuthProvider>
       <App />
     </AuthProvider>
-  </StrictMode>,
-)
+  </StrictMode>
+);
